56750675d47de39328e2ecd275a217b72b5d5661
Jen Cant <jennifer.cant@gmail.com>
2017-07-30 01:48:51 +0100

    get data from news api, loop through titles

--- dist/index.php
+++ dist/index.php
@@ -4,7 +4,17 @@
   </head>
   <body>
   	<main id="app">
+
   		<h1>{{ message }}</h1>
+
+  		<h2>Feed source: {{ source }}</h2>
+ 
+  		<pre v-bind="getData()"></pre>
+  		<div class="articles" v-for="newsPost in newsFeed.articles" >
+  			
+  		<h3>{{newsPost.title}}</h3>
+
+  		</div>
   	</main>
     <script src="app.js"></script>
   </body>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
+import config from '../config/config.js';
+
+var apikey = config.apikey;
+
+Vue.config.devtools = true;
+Vue.use(VueResource);
 
 new Vue({
 	el: '#app',
 	data: {
-		message: "Hello world"
+		newsFeed: ""
 	},
 	computed: {
-		test: function(){
-			return 'test';
+		source: function() {
+			var source = this.newsFeed.source;
+			return source;
 		}
-	},
-	methods: {
+	}, 
+	methods: 
+	{
 		getData: function() {
-
-			 }
+			var newsFeed = this.$http.get('https://newsapi.org/v1/articles?source=the-next-web&sortby=latest&apikey='+ apikey).then(response => {
+				this.newsFeed = response.body;
+			}, response => {
+				console.error(error);
+			});
 		},
+	}
 });
