b9d5894eafb11e393dfd71b529e22a54a5b0dd86
Jen Cant <jennifer.cant@gmail.com>
2017-07-30 17:32:01 +0100

    installed bulmaCSS framework, began basic styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,26 @@ var path 	= require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-resolve: {
-  alias: {
-    'vue$': 'vue/dist/vue.js',
-    'jquery': "jquery/src/jquery"
-  }
-}
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader" // creates style nodes from JS strings 
+			}, {
+				loader: "css-loader" // translates CSS into CommonJS 
+			}, {
+				loader: "sass-loader" // compiles Sass to CSS 
+			}]
+		}]
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue.js',
+		},
+	}
 };
